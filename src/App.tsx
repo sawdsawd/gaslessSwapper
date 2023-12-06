4e1f3db52960ee191f6e102dcfa49ca958e6bdc6
sawdsawd <leonardobog@gmail.com>
2023-12-06 10:23:08 +0100

    useffect fix

--- src/App.tsx
+++ src/App.tsx
@@ -32,7 +32,6 @@ async function connect() {
 }
 
 function App() {
-  const [ready, setReady] = useState(false);
   const [account1, setAccount1] = useState<string>('');
   const [account2, setAccount2] = useState<string>('');
 
@@ -41,61 +40,46 @@ function App() {
   const ctfContract = useRef<Contract | null>(null);
 
   useEffect(() => {
-    const init = async () => {
+    const initContracts = async () => {
       const selectedAccount = await connect();
 
-      if (selectedAccount) {
-        setAccount1(selectedAccount);
-
-        // const { gsnSigner } = await RelayProvider.newEthersV6Provider({
-        //   provider: new ethers.providers.Web3Provider((window as any).ethereum),
-        //   config: {
-        //     paymasterAddress: acceptEverythingPaymasterGoerli,
-        //   },
-        // });
-
-        // const ethersProvider = new ethers.providers.Web3Provider((window as any).ethereum);
-        // const signer = await ethersProvider.getSigner();
-
-        // console.log('RelayProvider init success');
-
-        // daiContract.current = new ethers.Contract(daiContractAddress, JSON.stringify(daiAbi), gsnSigner);
-        // swapperContract.current = new ethers.Contract(swapperContractAddress, JSON.stringify(swapperAbi), gsnSigner);
+      if (!selectedAccount) {
+        console.log('No selected account');
+        return;
       }
-      
+
+      setAccount1(selectedAccount);
+
+      // Initialize daiContract and swapperContract here if needed
+
       const gsnConfig: Partial<GSNConfig> = {
         loggerConfiguration: { logLevel: 'debug' },
-        paymasterAddress: PaymasterType.PermitERC20UniswapV3Paymaster
-      }
-  
-      const gsnProvider = TokenPaymasterProvider.newProvider({ 
-        provider: (window as any).ethereum, 
-        config: gsnConfig 
-      })    
-      await gsnProvider.init(daiContractAddress)
-      console.log('GSN Provider:', gsnProvider);
+        paymasterAddress: PaymasterType.PermitERC20UniswapV3Paymaster,
+      };
 
-      const provider2 = new ethers.providers.Web3Provider(gsnProvider)
+      const gsnProvider = TokenPaymasterProvider.newProvider({
+        provider: (window as any).ethereum,
+        config: gsnConfig,
+      });
+      await gsnProvider.init(daiContractAddress);
+
+      const provider2 = new ethers.providers.Web3Provider(gsnProvider);
       console.log('Ethers Provider:', provider2);
 
-      console.log('DAI Balance of account: ', await provider2.getBalance(account1))
-  
+      console.log('DAI Balance of account: ', await provider2.getBalance(selectedAccount));
+
       const signer = provider2.getSigner();
       console.log('Signer:', signer);
 
       // daiContract.current = new ethers.Contract(daiContractAddress, JSON.stringify(daiAbi), signer);
 
       ctfContract.current = new ethers.Contract(ctfContractAddress, JSON.stringify(ctfAbi), signer);
-
-      setReady(true);
     };
 
-    init();
+    initContracts();
   }, []);
 
-  const send = async function () {
-    // await daiContract.current?.transferFrom(account1, account2, ethers.utils.parseEther("20"))
-    // console.log("Current holder: " + await ctfContract.current?.currentHolder())
+  const capture = async function () {
     const tx = await ctfContract.current?.captureTheFlag();
     await tx.wait()
   }
@@ -116,7 +100,8 @@ function App() {
         Enter Account2 Address:
         <input type="text" value={(account2)} onChange={(e) => setAccount2(e.target.value)} />
       </label>
-      <button onClick={send}>Send</button>
+      <br/>
+      <button onClick={capture}>Capture</button>
     </>
   );
 }
